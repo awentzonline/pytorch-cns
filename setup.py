62b41fe37eb2d12132b60b208ba0e17559ef1acd
Adam Wentz <adam@adamwentz.com>
2017-08-07 23:10:19 -0700

    bump 0.2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='pytorch-cns',
-    version='0.2.2',
+    version='0.2.3',
     description='Generalized Compressed Network Search with PyTorch',
     author='Adam Wentz',
     author_email='adam@adamwentz.com',
